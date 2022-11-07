2a4b0488e5ccf5d9d454811e3d123a9c0c381eeb
Kaninchen <16620882+SuperKaninchen@users.noreply.github.com>
2022-11-07 16:42:01 +0100

    Add files via upload

--- MyGame.py
+++ MyGame.py
@@ -5,12 +5,13 @@ size = width, height = 800, 600
 
 class Ball(object):
     def __init__(self):
-        self.velocity = [1, 1]
+        self.count = 0
+        self.velocity = [100, 86]
         self.rect = pygame.Rect((100, 50), (50, 50))
         self.surface = pygame.Surface((self.rect.width, self.rect.height))
 
     def move(self, delta):
-        self.rect = self.rect.move(self.velocity[0]*delta, self.velocity[1]*delta)
+        self.rect = self.rect.move(self.velocity[0]/delta, self.velocity[1]/delta)
 
 
 def update(ball, delta, sound):
@@ -18,14 +19,17 @@ def update(ball, delta, sound):
     if ball.rect.left < 0 or ball.rect.right > width:
         ball.velocity[0] = ball.velocity[0]*-1
         sound.play()
+        ball.count += 1
     if ball.rect.top < 0 or ball.rect.bottom > height:
         ball.velocity[1] = ball.velocity[1]*-1
         sound.play()
+        ball.count += 1
 
 
-def show(screen, ball):
-    screen.fill((0, 0, 50))
+def show(screen, ball, font):
+    screen.fill((0, 0, 0))
     screen.blit(ball.surface, ball.rect)
+    screen.blit(font, (32, 32))
     pygame.display.flip()
 
 def main():
@@ -37,7 +41,9 @@ def main():
 
     clock = pygame.time.Clock()
 
-    sound = pygame.mixer.Sound("OBAMNA.mp3")
+    sound = pygame.mixer.Sound("click.mp3")
+
+    font = pygame.font.Font(None, 32)
 
     ball = Ball()
     pygame.draw.circle(ball.surface, (255, 255, 255), (25, 25), 25)
@@ -54,7 +60,9 @@ def main():
 
         delta = clock.get_time()
         update(ball, delta, sound)
-        show(screen, ball)
+
+        font_surface = font.render(f"Hits: {ball.count}", True, (255, 255, 255))
+        show(screen, ball, font_surface)
 
 
 if __name__ == "__main__":
